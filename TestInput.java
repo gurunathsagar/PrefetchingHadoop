2829340814fb652cb5b120e2a9ed9052c64868d8
gurunathsagar <gurunath.sagar@gmail.com>
2016-04-11 21:16:15 -0400

    File writing into shell script to copy hadoop. Command line argument needs to be implemented.

--- TestInput.java
+++ TestInput.java
@@ -8,11 +8,13 @@ class FileDescription
 {
 	private String name;
 	public long length;
+	public int tag;
 
 FileDescription(String name, long length)
 {
 	this.name=name;
 	this.length=length;
+	this.tag=0;
 }
 
 public String getName()
@@ -43,6 +45,7 @@ List<FileDescription> results = new ArrayList<FileDescription>();
 
 File[] files = new File(".").listFiles();
 //If this pathname does not denote a directory, then listFiles() returns null. 
+OutputStream out = null;
 
 for (File file : files) {
     if (file.isFile()) {
@@ -53,18 +56,42 @@ for (File file : files) {
 Collections.sort(results, new Comparator<FileDescription>() {
     @Override
     public int compare(FileDescription z1, FileDescription z2) {
-        if (z1.length > z2.length)
-            return 1;
         if (z1.length < z2.length)
+            return 1;
+        if (z1.length > z2.length)
             return -1;
         return 0;
     }
 });
 
+
+try
+{
+File newFile = new File(".", "output.txt");
+newFile.createNewFile();
+String str = "#!/bin/bash" + "\n\n";
+
+FileWriter fw = new FileWriter(newFile.getAbsoluteFile());
+BufferedWriter writer = new BufferedWriter(fw);
+
+writer.write(str);
+//	out = new FileOutputStream("output.txt");
+//		out.write(str);
+	
 for (FileDescription temp : results) {
-		System.out.println(temp.getName());	
+		//System.out.println(temp.getName());	
+	if(temp.tag==0)
+		{
+			writer.write( "hadoop fs -put " + temp.getName() + " /user/gurunath/input " + "\n");
+			temp.tag=1;
+		}
+	
 }
 
+writer.close();
+	}catch(IOException e){
+      System.out.print("Exception");
+   }
 }
 }
 
