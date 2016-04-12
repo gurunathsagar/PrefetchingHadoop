b10b1428eb22d2d9d192c4bdcbf965eb3aeec8e9
gurunathsagar <gurunath.sagar@gmail.com>
2016-04-11 22:14:36 -0400

    The script requires input dir as param1 and output in hdfs as param2

--- call.sh
+++ call.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 javac TestInput.java
-java TestInput /home/hduser/input_file /user/gurunath/temp
+#java TestInput /home/hduser/input_file /user/gurunath/temp
+java TestInput $1 $2
+chmod +x output.sh
+./output.sh
+rm output.sh
